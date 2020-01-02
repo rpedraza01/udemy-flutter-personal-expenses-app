76c5451e2c5430428da9c54d50e9dcf2c82a761b
rpedraza01 <rpedraza01@gmail.com>
2020-01-02 15:24:01 -0800

    Slight scrolling UI improvement

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 450,
+      height: 500,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
